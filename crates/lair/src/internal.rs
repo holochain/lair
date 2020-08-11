6bcc5d8db0961aaad9b1f12943ca999729a07ec2
neonphog <neonphog@gmail.com>
2020-08-11 17:07:48 -0600

    checkpoint fileformat

--- crates/lair/src/internal.rs
+++ crates/lair/src/internal.rs
@@ -1,6 +1,7 @@
 //! Internal utility functions - note, the api for anything in this module
 //! is unstable and may change even for patch versions of this library.
 
+pub mod codec;
 pub mod pid_check;
 pub mod sign_ed25519;
 pub mod tls;
