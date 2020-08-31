f0ba54402e883e53634b29f058c7716fc2473dc8
neonphog <neonphog@gmail.com>
2020-08-31 09:59:36 -0600

    add cli args to bin

--- crates/lair_keystore/src/bin/lair-keystore/main.rs
+++ crates/lair_keystore/src/bin/lair-keystore/main.rs
@@ -2,9 +2,34 @@
 #![deny(missing_docs)]
 //! main entry point
 
+use structopt::StructOpt;
+
+#[derive(Debug, StructOpt)]
+#[structopt(name = "lair-keystore")]
+struct Opt {
+    /// Print out version info and exit.
+    #[structopt(short, long)]
+    version: bool,
+
+    /// Set the lair data directory.
+    #[structopt(short = "d", long, env = "LAIR_DIR")]
+    lair_dir: Option<std::path::PathBuf>,
+}
+
 /// main entry point
 #[tokio::main]
 pub async fn main() -> lair_keystore_api::LairResult<()> {
+    let opt = Opt::from_args();
+
+    if opt.version {
+        println!("lair-keystore {}", lair_keystore::LAIR_VER);
+        return Ok(());
+    }
+
+    if let Some(lair_dir) = opt.lair_dir {
+        std::env::set_var("LAIR_DIR", lair_dir);
+    }
+
     lair_keystore::execute_lair().await?;
 
     // wait forever... i.e. until a ctrl-c
