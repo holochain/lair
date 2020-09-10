53ca9d281edb4eed43991a57a11bd9236d270989
neonphog <neonphog@gmail.com>
2020-09-10 14:43:17 -0600

    test keystore tests

--- crates/lair_keystore_api/src/test.rs
+++ crates/lair_keystore_api/src/test.rs
@@ -43,7 +43,7 @@ pub struct FixtureTlsCert {
 
 /// Spawn a test keystore using publicly available private keys.
 /// DANGER - Not for production!
-pub async fn spawn_client_ipc(
+pub async fn spawn_test_keystore(
     fixture_sign_ed25519_keypairs: Vec<FixtureSignEd25519Keypair>,
     fixture_tls_certs: Vec<FixtureTlsCert>,
 ) -> LairResult<(
@@ -369,5 +369,174 @@ impl LairClientApiHandler for Internal {
                 .boxed()
                 .into(),
         )
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const PUB1: &[u8] = &[
+        154, 185, 40, 0, 115, 213, 127, 247, 174, 124, 110, 222, 11, 151, 230,
+        233, 2, 171, 91, 154, 79, 50, 137, 45, 188, 110, 75, 56, 45, 18, 156,
+        158,
+    ];
+    const SEC1: &[u8] = &[
+        207, 84, 35, 155, 191, 10, 211, 240, 254, 92, 222, 153, 125, 241, 80,
+        102, 189, 217, 201, 140, 112, 159, 21, 148, 138, 41, 85, 90, 169, 56,
+        174, 72,
+    ];
+    const PUB2: &[u8] = &[
+        123, 88, 252, 103, 102, 190, 254, 104, 167, 210, 29, 41, 26, 225, 12,
+        113, 137, 104, 253, 93, 101, 214, 107, 125, 58, 208, 110, 203, 2, 166,
+        30, 88,
+    ];
+    const SEC2: &[u8] = &[
+        59, 31, 135, 117, 115, 107, 84, 52, 95, 216, 51, 180, 79, 81, 14, 169,
+        163, 149, 166, 174, 167, 143, 3, 211, 123, 224, 24, 25, 201, 40, 81,
+        188,
+    ];
+
+    const CERT_SNI: &str = "ar1J-HVz0EO4CzS9CN8EFta.ad471maBa70w5vn6nNilfUa";
+    const CERT_SEC: &[u8] = &[
+        48, 83, 2, 1, 1, 48, 5, 6, 3, 43, 101, 112, 4, 34, 4, 32, 135, 101, 23,
+        181, 167, 183, 114, 94, 169, 84, 144, 224, 192, 41, 112, 118, 149, 226,
+        42, 187, 247, 210, 54, 43, 83, 125, 13, 209, 93, 207, 33, 153, 161, 35,
+        3, 33, 0, 83, 74, 255, 70, 132, 118, 51, 92, 85, 250, 176, 123, 49,
+        206, 237, 79, 161, 136, 99, 44, 52, 128, 94, 174, 55, 174, 198, 113,
+        79, 135, 111, 26,
+    ];
+    const CERT: &[u8] = &[
+        48, 130, 1, 48, 48, 129, 227, 160, 3, 2, 1, 2, 2, 1, 42, 48, 5, 6, 3,
+        43, 101, 112, 48, 33, 49, 31, 48, 29, 6, 3, 85, 4, 3, 12, 22, 114, 99,
+        103, 101, 110, 32, 115, 101, 108, 102, 32, 115, 105, 103, 110, 101,
+        100, 32, 99, 101, 114, 116, 48, 32, 23, 13, 55, 53, 48, 49, 48, 49, 48,
+        48, 48, 48, 48, 48, 90, 24, 15, 52, 48, 57, 54, 48, 49, 48, 49, 48, 48,
+        48, 48, 48, 48, 90, 48, 33, 49, 31, 48, 29, 6, 3, 85, 4, 3, 12, 22,
+        114, 99, 103, 101, 110, 32, 115, 101, 108, 102, 32, 115, 105, 103, 110,
+        101, 100, 32, 99, 101, 114, 116, 48, 42, 48, 5, 6, 3, 43, 101, 112, 3,
+        33, 0, 83, 74, 255, 70, 132, 118, 51, 92, 85, 250, 176, 123, 49, 206,
+        237, 79, 161, 136, 99, 44, 52, 128, 94, 174, 55, 174, 198, 113, 79,
+        135, 111, 26, 163, 62, 48, 60, 48, 58, 6, 3, 85, 29, 17, 4, 51, 48, 49,
+        130, 47, 97, 114, 49, 74, 45, 72, 86, 122, 48, 69, 79, 52, 67, 122, 83,
+        57, 67, 78, 56, 69, 70, 116, 97, 46, 97, 100, 52, 55, 49, 109, 97, 66,
+        97, 55, 48, 119, 53, 118, 110, 54, 110, 78, 105, 108, 102, 85, 97, 48,
+        5, 6, 3, 43, 101, 112, 3, 65, 0, 211, 114, 220, 25, 145, 60, 41, 144,
+        219, 0, 170, 31, 206, 39, 134, 136, 147, 103, 63, 215, 239, 108, 28,
+        136, 102, 40, 213, 247, 233, 32, 190, 66, 155, 175, 6, 206, 193, 223,
+        93, 244, 11, 54, 81, 66, 31, 79, 20, 161, 138, 83, 58, 13, 4, 214, 204,
+        189, 12, 66, 180, 147, 202, 208, 242, 3,
+    ];
+    const CERT_DIGEST: &[u8] = &[
+        112, 155, 175, 48, 124, 184, 87, 220, 71, 56, 229, 88, 125, 146, 177,
+        13, 218, 216, 23, 59, 225, 6, 23, 207, 126, 223, 169, 142, 92, 242,
+        240, 239,
+    ];
+
+    async fn setup() -> LairResult<ghost_actor::GhostSender<LairClientApi>> {
+        let (api, _evt) = spawn_test_keystore(
+            vec![
+                FixtureSignEd25519Keypair {
+                    pub_key: PUB1.to_vec(),
+                    priv_key: SEC1.to_vec(),
+                },
+                FixtureSignEd25519Keypair {
+                    pub_key: PUB2.to_vec(),
+                    priv_key: SEC2.to_vec(),
+                },
+            ],
+            vec![FixtureTlsCert {
+                priv_key_der: CERT_SEC.to_vec(),
+                sni: CERT_SNI.to_string(),
+                cert_der: CERT.to_vec(),
+                cert_digest: CERT_DIGEST.to_vec(),
+            }],
+        )
+        .await?;
+        Ok(api)
+    }
+
+    #[tokio::test(threaded_scheduler)]
+    async fn test_test_keystore_signing() -> LairResult<()> {
+        let api = setup().await?;
+        let api2 = api.clone();
+
+        let sys_info = api.lair_get_server_info().await?;
+        assert_eq!(crate::LAIR_VER, sys_info.version);
+        let sys_info = api2.lair_get_server_info().await?;
+        assert_eq!(crate::LAIR_VER, sys_info.version);
+
+        let (idx1, pk1) = api.sign_ed25519_new_from_entropy().await?;
+        assert_eq!(pk1.0.as_slice(), PUB1);
+        assert_eq!(pk1, api.sign_ed25519_get(idx1).await?);
+        let (idx2, pk2) = api2.sign_ed25519_new_from_entropy().await?;
+        assert_eq!(pk2.0.as_slice(), PUB2);
+        assert_eq!(pk2, api2.sign_ed25519_get(idx2).await?);
+        let (idx3, pk3) = api.sign_ed25519_new_from_entropy().await?;
+        assert_eq!(pk3, api.sign_ed25519_get(idx3).await?);
+        assert_ne!(pk3.0.as_slice(), PUB1);
+        assert_ne!(pk3.0.as_slice(), PUB2);
+
+        assert_eq!(idx3, api.lair_get_last_entry_index().await?);
+
+        let data = std::sync::Arc::new(b"test-data".to_vec());
+
+        let sig1 = api.sign_ed25519_sign_by_index(idx1, data.clone()).await?;
+        let sig2 = api
+            .sign_ed25519_sign_by_pub_key(pk1.clone(), data.clone())
+            .await?;
+        assert_eq!(sig1, sig2);
+        assert!(pk1.verify(data.clone(), sig1).await?);
+
+        let sig1 = api2.sign_ed25519_sign_by_index(idx2, data.clone()).await?;
+        let sig2 = api2
+            .sign_ed25519_sign_by_pub_key(pk2.clone(), data.clone())
+            .await?;
+        assert_eq!(sig1, sig2);
+        assert!(pk2.verify(data.clone(), sig1).await?);
+
+        let sig1 = api.sign_ed25519_sign_by_index(idx3, data.clone()).await?;
+        let sig2 = api
+            .sign_ed25519_sign_by_pub_key(pk3.clone(), data.clone())
+            .await?;
+        assert_eq!(sig1, sig2);
+        assert!(pk3.verify(data.clone(), sig1).await?);
+
+        Ok(())
+    }
+
+    #[tokio::test(threaded_scheduler)]
+    async fn test_test_keystore_tls() -> LairResult<()> {
+        let api = setup().await?;
+
+        let (idx1, sni1, digest1) = api
+            .tls_cert_new_self_signed_from_entropy(TlsCertOptions::default())
+            .await?;
+        assert_eq!(CERT_SNI, sni1.as_str());
+        assert_eq!(CERT_DIGEST, digest1.as_slice());
+
+        let (sni1, digest1) = api.tls_cert_get(idx1).await?;
+        assert_eq!(CERT_SNI, sni1.as_str());
+        assert_eq!(CERT_DIGEST, digest1.as_slice());
+
+        let cert1 = api.tls_cert_get_cert_by_index(idx1).await?;
+        let cert2 = api.tls_cert_get_cert_by_sni(sni1.clone()).await?;
+        let cert3 = api.tls_cert_get_cert_by_digest(digest1.clone()).await?;
+        assert_eq!(cert1, cert2);
+        assert_eq!(cert2, cert3);
+
+        let priv1 = api.tls_cert_get_priv_key_by_index(idx1).await?;
+        let priv2 = api.tls_cert_get_priv_key_by_sni(sni1).await?;
+        let priv3 = api.tls_cert_get_priv_key_by_digest(digest1).await?;
+        assert_eq!(priv1, priv2);
+        assert_eq!(priv2, priv3);
+
+        let (_idx2, sni2, digest2) = api
+            .tls_cert_new_self_signed_from_entropy(TlsCertOptions::default())
+            .await?;
+        assert_ne!(CERT_SNI, sni2.as_str());
+        assert_ne!(CERT_DIGEST, digest2.as_slice());
+
+        Ok(())
     }
 }
