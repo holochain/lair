421afe4c5840ed390a942b61919c34c5a8ac32ac
Connor Turland <connor@sprillow.com>
2021-04-29 21:28:24 -0400

    Update main.rs

--- crates/lair_keystore/src/bin/lair-keystore/main.rs
+++ crates/lair_keystore/src/bin/lair-keystore/main.rs
@@ -6,7 +6,15 @@ use structopt::StructOpt;
 use tracing::*;
 
 #[derive(Debug, StructOpt)]
-#[structopt(name = "lair-keystore")]
+#[structopt(
+    name = "lair-keystore"
+    help = "lair-keystore runs an IPC process for handling the most private data (like private keys) on behalf of Holochain with a very high regard for security.
+    One and only one `holochain` instance should talk to one `lair-keystore` instance.
+    lair-keystore runs tied to a configurable directory.
+    If multiple lair-keystore instances are run they must be run from separate directories.
+    A platform specific directory will be used as a default which will be equal to directories::ProjectDirs::from(\"host\", \"Holo\", \"Lair\")
+    https://docs.rs/directories/3.0.2/directories/struct.ProjectDirs.html#examples"
+)]
 struct Opt {
     /// Print out version info and exit.
     #[structopt(short, long)]
