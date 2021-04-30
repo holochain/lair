2290cdc1e8b52d43ed3db910364f622e4c9c37ae
Connor Turland <connor@sprillow.com>
2021-04-29 20:43:34 -0400

    Update main.rs

--- crates/lair_keystore/src/bin/lair-keystore/main.rs
+++ crates/lair_keystore/src/bin/lair-keystore/main.rs
@@ -13,7 +13,12 @@ struct Opt {
     version: bool,
 
     /// Set the lair data directory.
-    #[structopt(short = "d", long, env = "LAIR_DIR", help = "lair will run as an IPC process using this directory as its root for all aspects of persistence. a platform specific dir will be used as a default equal to directories::ProjectDirs::from(\"host\", \"Holo\", \"Lair\")"))]
+    #[structopt(
+        short = "d",
+        long,
+        env = "LAIR_DIR",
+        help = "lair will run as an IPC process using this directory as its root for all aspects of persistence. a platform specific dir will be used as a default equal to directories::ProjectDirs::from(\"host\", \"Holo\", \"Lair\")"
+    )]
     lair_dir: Option<std::path::PathBuf>,
 }
 
