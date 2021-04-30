c9d8dfa5e363a97c7df0aacc4c9f3382cfa787d2
neonphog <neonphog@gmail.com>
2021-04-30 09:25:33 -0600

    fmt

--- crates/lair_keystore/src/lib.rs
+++ crates/lair_keystore/src/lib.rs
@@ -28,7 +28,7 @@ pub async fn execute_lair() -> LairResult<()> {
     }
 
     let config = config.build();
-    
+
     println!("#lair-keystore-dir:{:?}#", config.get_root_path());
 
     let internal::pid_check::PidCheckResult { store_file } =
