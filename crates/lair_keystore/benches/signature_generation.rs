498900bfc0f76f7cffe9a87df10ad103e6eea633
neonphog <neonphog@gmail.com>
2021-06-22 09:47:45 -0600

    dep updates

--- crates/lair_keystore/benches/signature_generation.rs
+++ crates/lair_keystore/benches/signature_generation.rs
@@ -5,8 +5,14 @@ use lair_keystore_api::*;
 use once_cell::sync::Lazy;
 use std::sync::Arc;
 
+static TOKIO: Lazy<tokio::runtime::Runtime> = Lazy::new(|| {
+    tokio::runtime::Builder::new_multi_thread()
+        .enable_all()
+        .build()
+        .unwrap()
+});
+
 struct BenchStatic {
-    pub tokio: tokio::runtime::Handle,
     pub tmpdir: tempfile::TempDir,
     pub api_send: ghost_actor::GhostSender<LairClientApi>,
     pub sign_idx: KeystoreIndex,
@@ -14,62 +20,44 @@ struct BenchStatic {
 
 impl BenchStatic {
     pub fn new() -> Self {
-        let mut runtime = tokio::runtime::Builder::new()
-            .enable_all()
-            .threaded_scheduler()
-            .build()
-            .unwrap();
+        let (tmpdir, api_send, sign_idx) = TOKIO.block_on(async move {
+            let tmpdir = tempfile::tempdir().unwrap();
+            std::env::set_var("LAIR_DIR", tmpdir.path());
 
-        let tokio = runtime.handle().clone();
+            lair_keystore::execute_lair().await.unwrap();
 
-        std::thread::spawn(move || {
-            runtime.block_on(async move {
-                futures::future::pending::<()>().await;
-            });
-        });
+            let config = Config::builder().set_root_path(tmpdir.path()).build();
 
-        let (tmpdir, api_send, sign_idx) = tokio.enter(move || {
-            futures::executor::block_on(async move {
-                let tmpdir = tempfile::tempdir().unwrap();
-                std::env::set_var("LAIR_DIR", tmpdir.path());
+            let (api_send, mut evt_recv) =
+                ipc::spawn_client_ipc(config).await.unwrap();
 
-                lair_keystore::execute_lair().await.unwrap();
-
-                let config =
-                    Config::builder().set_root_path(tmpdir.path()).build();
-
-                let (api_send, mut evt_recv) =
-                    ipc::spawn_client_ipc(config).await.unwrap();
-
-                tokio::task::spawn(async move {
-                    while let Some(msg) = evt_recv.next().await {
-                        match msg {
-                            LairClientEvent::RequestUnlockPassphrase {
-                                respond,
-                                ..
-                            } => {
-                                respond.respond(Ok(async move {
-                                    Ok("passphrase".to_string())
-                                }
-                                .boxed()
-                                .into()));
+            tokio::task::spawn(async move {
+                while let Some(msg) = evt_recv.next().await {
+                    match msg {
+                        LairClientEvent::RequestUnlockPassphrase {
+                            respond,
+                            ..
+                        } => {
+                            respond.respond(Ok(async move {
+                                Ok("passphrase".to_string())
                             }
+                            .boxed()
+                            .into()));
                         }
                     }
-                });
+                }
+            });
 
-                let info = api_send.lair_get_server_info().await.unwrap();
-                assert_eq!("lair-keystore", &info.name);
+            let info = api_send.lair_get_server_info().await.unwrap();
+            assert_eq!("lair-keystore", &info.name);
 
-                let (sign_idx, _sign_pub_key) =
-                    api_send.sign_ed25519_new_from_entropy().await.unwrap();
+            let (sign_idx, _sign_pub_key) =
+                api_send.sign_ed25519_new_from_entropy().await.unwrap();
 
-                (tmpdir, api_send, sign_idx)
-            })
+            (tmpdir, api_send, sign_idx)
         });
 
         Self {
-            tokio,
             tmpdir,
             api_send,
             sign_idx,
@@ -81,11 +69,12 @@ static STATIC: Lazy<Arc<BenchStatic>> =
     Lazy::new(|| Arc::new(BenchStatic::new()));
 
 fn sign_small() {
-    STATIC.tokio.block_on(async move {
-        let _result = STATIC
+    let bench = &*STATIC;
+    TOKIO.block_on(async move {
+        let _result = bench
             .api_send
             .sign_ed25519_sign_by_index(
-                STATIC.sign_idx,
+                bench.sign_idx,
                 black_box(vec![0xdb; 32].into()),
             )
             .await
