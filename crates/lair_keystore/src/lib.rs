85ac306f1e13a6c78fecaeec7907a18e00dfe0de
zo-el <joelulahanna@gmail.com>
2021-07-05 20:41:30 +0530

    lair_keystore: execute_gen fn to init gen option

--- crates/lair_keystore/src/lib.rs
+++ crates/lair_keystore/src/lib.rs
@@ -12,6 +12,7 @@ include!(concat!(env!("OUT_DIR"), "/ver.rs"));
 
 use lair_keystore_api::*;
 use std::sync::Arc;
+use crate::store::EntryStoreSender;
 
 pub mod internal;
 
@@ -38,3 +39,30 @@ pub async fn execute_lair() -> LairResult<()> {
 
     Ok(())
 }
+
+/// Gen loop of lair executable.
+pub async fn execute_gen() -> LairResult<()> {
+    let mut config = Config::builder();
+
+    if let Some(lair_dir) = std::env::var_os("LAIR_DIR") {
+        config = config.set_root_path(lair_dir);
+    }
+
+    let config = config.build();
+
+    println!("#lair-keystore-dir:{:?}#", config.get_root_path());
+
+    let internal::pid_check::PidCheckResult { store_file } =
+        internal::pid_check::pid_check(&config)?;
+
+    let store_actor =
+    store::spawn_entry_store_actor(config.clone(), store_file).await?;
+
+    let keypair: entry::EntrySignEd25519 = lair_keystore_api::internal::sign_ed25519::sign_ed25519_keypair_new_from_entropy().await?;
+
+    // TODO : handle the error here
+    store_actor.add_initial_sign_ed25519_keypair(keypair).await?;
+
+    Ok(())
+}
+
