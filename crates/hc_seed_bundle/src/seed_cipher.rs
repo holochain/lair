23a96e6a450e95c9c6f1283efd52fac842599e3a
neonphog <neonphog@gmail.com>
2021-09-01 16:03:56 -0600

    add seed cipher util code

--- crates/hc_seed_bundle/src/seed_cipher.rs
+++ crates/hc_seed_bundle/src/seed_cipher.rs
@@ -1,1 +1,20 @@
 //! A module for seed bundle cipher related items
+
+use sodoken::{SodokenError, SodokenResult};
+
+#[allow(dead_code)]
+mod u8array;
+use u8array::*;
+
+#[allow(dead_code)]
+mod seed_bundle;
+#[allow(unused_imports)]
+use seed_bundle::*;
+
+#[allow(dead_code)]
+mod pw_utils;
+#[allow(unused_imports)]
+use pw_utils::*;
+
+mod pw_hash_limits;
+pub use pw_hash_limits::*;
