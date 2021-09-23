a18c0cf635fe33ca98bcf01420fbd3e3149d5a9a
neonphog <neonphog@gmail.com>
2021-09-23 14:10:43 -0600

    new lair-keystore binary cli

---
+++ crates/lair_keystore/src/bin/lair-keystore-bin/cmd_server.rs
@@ -0,0 +1,34 @@
+use super::*;
+
+pub(crate) async fn exec(
+    config: LairServerConfig,
+    opt: OptServer,
+) -> LairResult<()> {
+    if opt.piped && opt.locked {
+        return Err(
+            "-p / --piped and -l / --locked are mutually exclusive".into()
+        );
+    }
+
+    // construct the server so that pid-check etc happens first
+    let server =
+        lair_keystore_lib::server::StandaloneServer::new(config).await?;
+
+    let passphrase = if opt.piped {
+        Some(read_piped_passphrase().await?)
+    } else if opt.locked {
+        None
+    } else {
+        Some(read_interactive_passphrase().await?)
+    };
+
+    if let Some(passphrase) = passphrase {
+        server.run_unlocked(passphrase).await?;
+    } else {
+        server.run_locked().await?;
+    }
+
+    // if we made it this far, the server is running...
+    // we want it to run forever, so this future never resolves:
+    futures::future::pending().await
+}
