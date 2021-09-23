809c687e002951d290abdde600b6312571c317e5
neonphog <neonphog@gmail.com>
2021-09-23 09:35:38 -0600

    encoding types to help with serialization / secrecy

--- crates/lair_keystore_api/src/lib.rs
+++ crates/lair_keystore_api/src/lib.rs
@@ -13,5 +13,12 @@ include!(concat!(env!("OUT_DIR"), "/ver.rs"));
 /// Lair Result Type
 pub type LairResult<T> = Result<T, one_err::OneErr>;
 
+pub mod encoding_types;
 pub mod internal;
 pub mod sodium_secretstream;
+
+/// re-export module of types generally used with lair
+pub mod prelude {
+    pub use crate::encoding_types::*;
+    pub use crate::LairResult;
+}
