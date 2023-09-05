595b2ec0f8bc5d865422426f1518a36bc401a5eb
Matt Gabrenya <matt@buildyourweb.app>
2023-09-05 13:48:07 -0700

    test: lair config supports absolute or relative paths for lair_root

--- crates/lair_keystore_api/tests/config.rs
+++ crates/lair_keystore_api/tests/config.rs
@@ -4,7 +4,7 @@ use lair_keystore_api::prelude::*;
 
 #[cfg(not(windows))]
 #[tokio::test(flavor = "multi_thread")]
-async fn lair_config_connection_url_absolute() {
+async fn lair_config_connection_url_relative_root() {
     use std::fs::{create_dir, remove_dir};
 
   let passphrase = sodoken::BufRead::from(&b"passphrase"[..]);
@@ -19,6 +19,29 @@ async fn lair_config_connection_url_absolute() {
   let mut expected_path: PathBuf = current_dir().unwrap();
   expected_path.push("config-root-path-test");
   let expected_path_str = expected_path.to_str().unwrap();
+
+  assert_eq!(true, config.connection_url.as_str().contains(expected_path_str));
+}
+
+#[cfg(not(windows))]
+#[tokio::test(flavor = "multi_thread")]
+async fn lair_config_connection_url_absolute_root() {
+    use std::fs::{create_dir, remove_dir};
+
+  let passphrase = sodoken::BufRead::from(&b"passphrase"[..]);
+
+  let _ = create_dir("./config-root-path-test");
+  let mut lair_root = current_dir().unwrap();
+  lair_root.push("config-root-path-test");
+  let config = hc_seed_bundle::PwHashLimits::Minimum
+      .with_exec(|| LairServerConfigInner::new(lair_root.clone(), passphrase.clone()))
+      .await
+      .unwrap();
+  let _ = remove_dir("./config-root-path-test");
+
+  let mut expected_path: PathBuf = current_dir().unwrap();
+  expected_path.push("config-root-path-test");
+  let expected_path_str = lair_root.to_str().unwrap();
 
   assert_eq!(true, config.connection_url.as_str().contains(expected_path_str));
 }
