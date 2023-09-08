bac7278352144505898b86f27afd5ff1e95cd22a
Matt Gabrenya <matt@buildyourweb.app>
2023-09-07 18:38:30 -0700

    refactor: use dunce::canonicalize instead of std canonicalize

--- crates/lair_keystore_api/src/config.rs
+++ crates/lair_keystore_api/src/config.rs
@@ -238,7 +238,7 @@ impl LairServerConfigInner {
             // on not-windows, we default to using unix domain sockets
             #[cfg(not(windows))]
             let connection_url = {
-                let mut con_path = root_path.clone().canonicalize().unwrap();
+                let mut con_path = dunce::canonicalize(root_path)?;
                 con_path.push("socket");
                 url::Url::parse(&format!(
                     "unix://{}?k={}",
